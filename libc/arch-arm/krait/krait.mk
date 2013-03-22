704dc66d5fe2feef12c75235545c38a2a92347fa
trailblazerz <juston@uoregon.edu>
2013-03-22 23:03:02 +0800

    Use Code aurora forum memcpy with krait optimizations

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -1,4 +1,4 @@
-$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy.S)
+$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy-krait.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/krait/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
 
